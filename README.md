1ddf351621c15ff34d0559dbd562d2236b0ef8d0
jsdrever <jsdrever09@gmail.com>
2023-05-06 15:39:27 -0500

    better svg logo maker

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ https://drive.google.com/file/d/1kGM6jsDVkPM9i4WGVGA2QuetFxyWA7PR/view
 
 ## setup and usage instructions
 the user will recieve three prompts to choose from. They will select options from a list for shape and color, then they must input three or less characters to render their new logo.
+- Requires Node v12+. (This app has only been tested on Node 16)
+- Run `npm install` to download dependencies.
+- Run `node index.js` to start the app.
+  - Answer questions to generate a logo.
+  - Logo will be written to `logo.svg` in the working directory.
+- Run tests with `npm run test`
 
 ## Link to Github
 https://github.com/jsdrever/svgforme.git
--- index.js
+++ index.js
@@ -1,74 +1,77 @@
-const inquirer = require("inquirer");
-// import inquirer from 'inquirer';
-const fs = require('fs');
-const SVG = require("./lib/svg")
-const {Circle,Square,Triangle} = require("./lib/shapes") 
-// these import from my other files and the components to write files and run tests
+// const inquirer = require("inquirer");
+// // import inquirer from 'inquirer';
+// const fs = require('fs');
+// const SVG = require("./lib/svg")
+// const {Circle,Square,Triangle} = require("./lib/shapes") 
+// // these import from my other files and the components to write files and run tests
 
-const questions = [
-    {
-        type: "list",
-        name: "shape",
-        message: "What shape would you like for the svg?",
-        choices: ['circle', 'square', 'triangle']
-    },
-    {
-        type: "list",
-        name: "color",
-        message: "What color would you like for the svg?",
-        choices: ['red', 'green', 'blue']
-    },
-    {
-        type: "input",
-        name: "text",
-        message: "What three or fewer letters would you like to display on the svg?"
-    },
-  // Selects the options for the svg
-]
-// This writes the file and puts the data on the file
-function writeToFile(fileName, data) { 
-    fs.writeFileSync(fileName, data, (err) =>
-      err ? console.error(err) : console.log('Success!')) 
-}
-// this function calls the questions/answers from inquirer
-function init() {
-    inquirer.prompt(questions)
-    .then((answers) => {
-        // .then(({ text, textColor, shapeType, shapeColor }) => {
-        // console.log(`The shape you chose is ${answers.shape}, the color you chose is ${answers.color}, and the text you're adding is ${answers.text}`);
+// const questions = [
+//     {
+//         type: "list",
+//         name: "shape",
+//         message: "What shape would you like for the svg?",
+//         choices: ['circle', 'square', 'triangle']
+//     },
+//     {
+//         type: "list",
+//         name: "color",
+//         message: "What color would you like for the svg?",
+//         choices: ['red', 'green', 'blue']
+//     },
+//     {
+//         type: "input",
+//         name: "text",
+//         message: "What three or fewer letters would you like to display on the svg?"
+//     },
+//   // Selects the options for the svg
+// ]
+// // This writes the file and puts the data on the file
+// function writeToFile(fileName, data) { 
+//     fs.writeFileSync(fileName, data, (err) =>
+//       err ? console.error(err) : console.log('Success!')) 
+// }
+// // this function calls the questions/answers from inquirer
+// function init() {
+//     inquirer.prompt(questions)
+//     .then((answers) => {
+//         // .then(({ text, textColor, shapeType, shapeColor }) => {
+//         // console.log(`The shape you chose is ${answers.shape}, the color you chose is ${answers.color}, and the text you're adding is ${answers.text}`);
         
-        let shapeCreated
-        switch (answers.shape){
-            case "circle":  
-                shapeCreated = new Circle()
-                break;
-            case "square":  
-                shapeCreated = new Square()
-                break;
-            case "triangle":  
-                shapeCreated = new Triangle()
-                break;
+//         let shapeCreated
+//         switch (answers.shape){
+//             case "circle":  
+//                 shapeCreated = new Circle()
+//                 break;
+//             case "square":  
+//                 shapeCreated = new Square()
+//                 break;
+//             case "triangle":  
+//                 shapeCreated = new Triangle()
+//                 break;
                  
         
-        }
-        shapeCreated.setColor(answers.color)
-        console.log(shapeCreated, 'here');
-        const svgArt = new SVG()
-        svgArt.setShape(shapeCreated)
-        svgArt.setText(answers.text)
-        console.log(svgArt);
+//         }
+//         shapeCreated.setColor(answers.color)
+//         console.log(shapeCreated, 'here');
+//         const svgArt = new SVG()
+//         svgArt.setShape(shapeCreated)
+//         svgArt.setText(answers.text)
+//         console.log(svgArt);
         
-        return writeToFile('logo.svg',svgArt.render())
+//         return writeToFile('logo.svg',svgArt.render())
         
-        // this applies all the answers and renders the text to the newly written file logo.svg
-    })
-    .then(() => {
-        console.log('we done it yo!');
-    }) 
-    .catch((error) => {
-        console.log(error);
-        console.log("wrong dummy");
-    });
-}
+//         // this applies all the answers and renders the text to the newly written file logo.svg
+//     })
+//     .then(() => {
+//         console.log('we done it yo!');
+//     }) 
+//     .catch((error) => {
+//         console.log(error);
+//         console.log("wrong dummy");
+//     });
+// }
 
-init();
+// init();
+const CLI = require("./lib/cli");
+
+new CLI().run();
